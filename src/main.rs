300e22b43f7cb48932bae30e3df632bb310c8e18
KaiFireborn <blitzmasterev3@gmail.com>
2026-05-15 17:19:43 +0200

    feat: kinda grid tiles; next step preview activation

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@
 
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use eframe::egui;
+use eframe::egui::{self, vec2};
 use std::path::Path;
 
 fn main() -> eframe::Result {
@@ -29,7 +29,7 @@ fn main() -> eframe::Result {
 
 struct MyApp {
     selected_index: Option<usize>,
-    is_preview_open: bool,
+    is_preview_open: bool, // TODO: make switchable with Space and also load selected image; Also arrow navigation
     image_paths: Vec<String>,
 }
 impl Default for MyApp {
@@ -44,79 +44,85 @@ impl Default for MyApp {
     }
 }
 
-fn wide_button(ui: &mut egui::Ui, text: impl Into<egui::WidgetText>, size: egui::Vec2) -> egui::Response {
+fn wide_button(
+    ui: &mut egui::Ui,
+    text: impl Into<egui::WidgetText>,
+    size: egui::Vec2,
+) -> egui::Response {
     ui.add_sized(size, egui::Button::new(text))
 }
 
 enum MediaType {
-        ImageGif,
-        Video,
-        Unknown
-    }
+    ImageGif,
+    Video,
+    Unknown,
+}
 
 fn get_media_filetype(path_in: &str) -> MediaType {
-    
     let path = Path::new(path_in);
-    let ext = path.extension()
+    let ext = path
+        .extension()
         .and_then(|s| s.to_str())
         .map(|s| s.to_lowercase());
     match ext.as_deref() {
         Some("jpg") | Some("jpeg") | Some("png") | Some("gif") | Some("webp") | Some("bmp") => {
             MediaType::ImageGif
         }
-        Some("mp4") | Some("mkv") | Some("mov") | Some("avi") | Some("webm") => {
-            MediaType::Video
-        }
+        Some("mp4") | Some("mkv") | Some("mov") | Some("avi") | Some("webm") => MediaType::Video,
         _ => MediaType::Unknown,
     }
 }
 
-fn draw_media(ui: &mut egui::Ui, path_in: &str) {
+fn draw_media(ui: &mut egui::Ui, path_in: &str, tile_size: egui::Vec2) {
     let filetype = get_media_filetype(path_in);
 
     let library_abs_path = "/home/kf/Files/Programming/lable";
     let uri = format!("file://{}/{}", library_abs_path, path_in);
     // TODO: in the end will actually have absolute paths, but the path still set at runtime and not like that
     match filetype {
-        MediaType::ImageGif => { ui.add(egui::Image::new(uri)); }
-        _ => { ui.label("Not an image"); }
+        MediaType::ImageGif => {
+            ui.add(
+                egui::Image::new(uri)
+                    .fit_to_exact_size(tile_size)
+                    .maintain_aspect_ratio(true), // .max_size(egui::vec2(150.0, 150.0))
+            );
+        }
+        _ => {
+            ui.label("Not an image");
+        }
     }
 }
 
 impl eframe::App for MyApp {
-
-    
-
     fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut eframe::Frame) {
+        // let audio_device = egui_video::AudioDevice::new()?;
 
-            // let audio_device = egui_video::AudioDevice::new()?;
-    
         egui::CentralPanel::default().show_inside(ui, |ui| {
             egui::Panel::left("left_panel")
                 .min_size(250.0)
                 .default_size(250.0)
                 .show_inside(ui, |ui| {
                     ui.label("Filter");
-                    egui::Panel::bottom("bottom_left_panel").min_size(360.0).show_inside(ui, |ui| {
-                        let button_size = egui::vec2(ui.available_width(), 35.0);
-                        ui.label("Utilities");
+                    egui::Panel::bottom("bottom_left_panel")
+                        .min_size(360.0)
+                        .show_inside(ui, |ui| {
+                            let button_size = egui::vec2(ui.available_width(), 35.0);
+                            ui.label("Utilities");
 
-                        //TODO: backend calls
-                        if  wide_button(ui, "Clear filters", button_size).clicked() { }
-                        if wide_button(ui, "Detect duplicates", button_size).clicked() { 
-                            // apply sys:duplicate:x or something
-                        }
-                        if wide_button(ui, "Infer tags", button_size).clicked() {
-                            // read sidecars, create tags, delete sidecars
-                         }
-                        if wide_button(ui, "Move to subolder", button_size).clicked() { 
-                            //subfolder name popup, mkdir, update db, do it
-                        }
-
-                    });
+                            //TODO: backend calls
+                            if wide_button(ui, "Clear filters", button_size).clicked() {}
+                            if wide_button(ui, "Detect duplicates", button_size).clicked() {
+                                // apply sys:duplicate:x or something
+                            }
+                            if wide_button(ui, "Infer tags", button_size).clicked() {
+                                // read sidecars, create tags, delete sidecars
+                            }
+                            if wide_button(ui, "Move to subolder", button_size).clicked() {
+                                //subfolder name popup, mkdir, update db, do it
+                            }
+                        });
                     egui::CentralPanel::default().show_inside(ui, |ui| {
                         ui.label("Filter tags");
-
                     });
                 });
 
@@ -125,33 +131,53 @@ impl eframe::App for MyApp {
                 .default_size(250.0)
                 .show_inside(ui, |ui| {
                     ui.label("Tags");
-                    egui::Panel::bottom("bottom_right_panel").min_size(360.0).show_inside(ui, |ui| {
-                        let button_size = egui::vec2(ui.available_width(), 35.0);
+                    egui::Panel::bottom("bottom_right_panel")
+                        .min_size(360.0)
+                        .show_inside(ui, |ui| {
+                            let button_size = egui::vec2(ui.available_width(), 35.0);
 
-                        ui.label("Image actions");
-                          //TODO: backend calls
-                        if wide_button(ui, "Open in default app", button_size).clicked() { }
-                        if wide_button(ui, "Open containing folder", button_size).clicked() { }
-                        if wide_button(ui, "Rename", button_size).clicked() { }
-                        if wide_button(ui, "Delete", button_size).clicked() {
-                            // Move to trash folder
-                         }
-                    });
+                            ui.label("Image actions");
+                            //TODO: backend calls
+                            if wide_button(ui, "Open in default app", button_size).clicked() {}
+                            if wide_button(ui, "Open containing folder", button_size).clicked() {}
+                            if wide_button(ui, "Rename", button_size).clicked() {}
+                            if wide_button(ui, "Delete", button_size).clicked() {
+                                // Move to trash folder
+                            }
+                        });
                     egui::CentralPanel::default().show_inside(ui, |ui| {
                         ui.label("Image tags");
-
-
                     });
                 });
 
             egui::CentralPanel::default().show_inside(ui, |ui| {
+                let preview_size = ui.available_size();
+
+                // TODO: make go OVER grid while still keeping it
+                if self.is_preview_open {
+                    draw_media(ui, "images/test1.gif", preview_size);
+                }
+
                 ui.vertical_centered(|ui| {
                     // let side = ui.available_height();
                     // let (rect, _) =
                     //     ui.allocate_at_least(egui::vec2(side, side), egui::Sense::hover());
 
                     // ui.painter().rect_filled(rect, 0.0, egui::Color32::GRAY);
-                    draw_media(ui, "images/test0.png")
+                    let tile_size = egui::vec2(221.0, 221.0); // TODO: make ajustable
+                    egui::ScrollArea::vertical().show(ui, |ui| {
+                        ui.horizontal_wrapped(|ui| {
+                            // Adjust spacing between tiles
+                            ui.spacing_mut().item_spacing = egui::vec2(24.0, 24.0);
+                            // TODO: make focusable
+                            draw_media(ui, "images/test0.png", tile_size);
+                            draw_media(ui, "images/test1.gif", tile_size);
+                            draw_media(ui, "images/test2.jpg", tile_size);
+                            draw_media(ui, "images/test3.png", tile_size);
+                            draw_media(ui, "images/test4.jpg", tile_size);
+                            draw_media(ui, "images/test5.gif", tile_size);
+                        });
+                    });
                 });
             });
 
